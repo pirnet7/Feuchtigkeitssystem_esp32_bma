59f88c8ea8408c618914b6c6aba41390dc6bc05f
Oliver Zenger <oliver@zenger.org>
2025-12-30 12:58:37 +0100

    feat(mqtt): add WiFi and MQTT reconnect logic

--- mqtt.ino
+++ mqtt.ino
@@ -1,63 +1,90 @@
-  // MQTT publish
-  //
-  // mqtt lib:      https://github.com/256dpi/arduino-mqtt
-  // mqtt provider: https://www.shiftr.io/try/
+// MQTT publish
+//
+// mqtt lib:      https://github.com/256dpi/arduino-mqtt
+// mqtt provider: https://www.shiftr.io/try/
 
-  //-------------------------------------------------------------------
-  #include <WiFi.h>
-  #include <MQTT.h>
-  #include "config.h"
+//-------------------------------------------------------------------
+#include <WiFi.h>
+#include <MQTT.h>
+#include "config.h"
 
-  //-------------------------------------------------------------------
-  WiFiClient wifinetwork;
-  MQTTClient mqttclient;
+//-------------------------------------------------------------------
+WiFiClient wifinetwork;
+MQTTClient mqttclient;
 
-  //-------------------------------------------------------------------
-  void setup(void)
+//-------------------------------------------------------------------
+void setup(void)
+{
+  //---
+  Serial.begin(115200);
+
+  //---
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to network ");
+
+  while (WiFi.status() != WL_CONNECTED)
   {
-    //---
-    Serial.begin(115200);
+    Serial.print(".");
+    delay(250);
+  }
+  Serial.println(" Done");
 
-    //---
+  //---
+  mqttclient.begin(wifinetwork);
+
+  mqttclient.setHost(MQTT_BROKER_URL);
+  Serial.print("Connecting to MQTT broker ");
+
+  while (mqttclient.connect(MQTT_CLIENT_ID, MQTT_BROKER_USER, MQTT_BROKER_PASSWORD) == false)
+  {
+    Serial.print(".");
+    delay(250);
+  }
+  Serial.println(" Done");
+}
+
+//-------------------------------------------------------------------
+void loop(void)
+{
+  static uint32_t zeitStempel = 0;
+
+  //---
+  mqttclient.loop();
+
+  //--- Reconnect bei Verbindungsverlust
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("WiFi lost! Reconnecting...");
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print("Connecting to network ");
-
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
       Serial.print(".");
       delay(250);
     }
-    Serial.println(" Done");
-
-    //---
-    mqttclient.begin(wifinetwork);
-
-    mqttclient.setHost(MQTT_BROKER_URL);
-    Serial.print("Connecting to MQTT broker ");
-
-    while (mqttclient.connect(MQTT_CLIENT_ID, MQTT_BROKER_USER, MQTT_BROKER_PASSWORD) == false) {
-      Serial.print(".");
-      delay(250);
-    }
-    Serial.println(" Done");
+    Serial.println(" WiFi reconnected");
   }
 
-  //-------------------------------------------------------------------
-  void loop(void)
+  if (!mqttclient.connected())
   {
-    static uint32_t zeitStempel = 0;
-    
-    //---
-    mqttclient.loop();
-
-    //---
-    // T=1000ms (1 Hz)
-    if ((millis() - zeitStempel) > 1000) {
-      zeitStempel = millis();
-
-      String topic = MQTT_TOPIC;
-      String payload = "Hello World";
-      mqttclient.publish(topic, payload);
-
-      Serial.println("Message published :-)");
+    Serial.println("MQTT lost! Reconnecting...");
+    while (!mqttclient.connect(MQTT_CLIENT_ID, MQTT_BROKER_USER, MQTT_BROKER_PASSWORD))
+    {
+      Serial.print(".");
+      delay(250);
     }
+    Serial.println(" MQTT reconnected");
   }
+
+  //---
+  // T=1000ms (1 Hz)
+  if ((millis() - zeitStempel) > 1000)
+  {
+    zeitStempel = millis();
+
+    String topic = MQTT_TOPIC;
+    String payload = "Hello World";
+    mqttclient.publish(topic, payload);
+
+    Serial.println("Message published :-)");
+  }
+}
